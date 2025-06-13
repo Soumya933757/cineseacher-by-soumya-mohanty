74e8f0210d8b366eb91230e46271c357f350744d
soumya933757 <soumyamohanty5555@gmail.com>
2025-06-13 11:40:45 +0530

    Displayed results in a list

--- src/components/HomePage/index.jsx
+++ src/components/HomePage/index.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
 
+import PageLoader from "components/commons/PageLoader";
 import { useShowMovies } from "hooks/reactQuery/moviesApi";
 import { Search } from "neetoicons";
 import { Input } from "neetoui";
 
+import MovieCard from "./MovieCard";
+
 const HomePage = () => {
   const [searchKey, setSearchKey] = useState("");
 
-  const { data: movies = {} } = useShowMovies(searchKey);
+  const { data: movies = {}, isFetching } = useShowMovies(searchKey);
   console.log("Movies data:", movies);
 
   return (
@@ -20,6 +23,27 @@ const HomePage = () => {
           value={searchKey}
           onChange={e => setSearchKey(e.target.value)}
         />
+      </div>
+      <div className="mt-10 h-full w-full">
+        {isFetching ? (
+          <PageLoader />
+        ) : (
+          <div className="h-full w-full">
+            {movies?.Search?.length > 0 ? (
+              <div className="flex h-full w-full flex-col justify-between gap-4">
+                <div className="grid grid-cols-1 grid-rows-2 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 ">
+                  {movies.Search.map((movie, index) => (
+                    <MovieCard key={index} movie={movie} />
+                  ))}
+                </div>
+              </div>
+            ) : (
+              <div className="flex h-full w-full items-center justify-center">
+                No movies found
+              </div>
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
