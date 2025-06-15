e1cf24612b49993e7729db4d3be2ed3c7c5f6ca4
soumya933757 <soumyamohanty5555@gmail.com>
2025-06-15 14:00:19 +0530

    Displayed favourite items

--- src/components/Favourite/FavouriteItem.jsx
+++ src/components/Favourite/FavouriteItem.jsx
@@ -1,5 +1,19 @@
 import React from "react";
 
-const FavouriteItem = () => <div className="">FavouriteItem</div>;
+import { Typography } from "neetoui";
+
+const FavouriteItem = ({ item }) => {
+  console.log(item);
+
+  return (
+    <div
+      className="md:h-18 flex h-14 w-3/5 items-center justify-between rounded-md border border-l-4 border-gray-200 px-5"
+      key={item.imdbID}
+    >
+      <Typography style="h6">{item.Title}</Typography>
+      <Typography style="h6">Rating: {item.imdbRating}/10</Typography>
+    </div>
+  );
+};
 
 export default FavouriteItem;
--- src/components/Favourite/index.jsx
+++ src/components/Favourite/index.jsx
@@ -1,9 +1,28 @@
 import React from "react";
 
-const Favourite = () => (
-  <div className="flex h-screen w-full flex-col items-center pt-16">
-    Favourite
-  </div>
-);
+import useFavouriteItemStore from "stores/useFavouriteItemStore";
+
+import FavouriteItem from "./FavouriteItem";
+
+const Favourite = () => {
+  const { favouriteItems } = useFavouriteItemStore();
+
+  return (
+    <div className="flex min-h-screen w-full flex-col items-center gap-4 pt-16">
+      {favouriteItems?.length > 0 ? (
+        favouriteItems.map(item => (
+          <FavouriteItem item={item} key={item.imdbID} />
+        ))
+      ) : (
+        <div
+          className="flex w-full items-center justify-center"
+          style={{ height: "80vh" }}
+        >
+          No Favourites Found
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Favourite;
